95bf48055a2221a9a2eddfd805ddef5a89e4466a
“yyy” <2237015207@qq.com>
2024-04-02 11:43:53 +0800

    fix3

--- 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -4,6 +4,7 @@ public class Algorithms {
     private Algorithms() {
         // Empty
     }
+
     public static <T extends Comparable<T>> void sort(T[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
@@ -23,99 +24,48 @@ public class Algorithms {
             array[array.length - 1 - i] = temp;
         }
     }
-    public static int binarySearch(int target, Integer[] array, int fromIndex, int toIndex) {
+
+    public static <T extends Comparable<T>> int binarySearch(T target, T[] array, int fromIndex, int toIndex) {
         int left = fromIndex;
         int right = toIndex;
         while (left <= right) {
             int mid = (right + left) / 2;
-            if(array[mid] == target){
-                return mid;
-            }else if(array[mid] < target){
-                left = mid + 1;
-            }else{
-                right = mid - 1;
-            }
-
-        }
-
-        return -1;
-    }
-    public static int binarySearch(String value, String[] array, int fromIndex, int toIndex) {
-        int left = fromIndex;
-        int right = toIndex;
-
-        while (left <= right) {
-            int mid = (right + left) / 2;
-            int cmp = value.compareTo(array[mid]);
-
+            int cmp = array[mid].compareTo(target);
             if (cmp == 0) {
                 return mid;
             } else if (cmp < 0) {
-                right = mid - 1;
-            } else {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-
         return -1;
     }
 
-
-    public static <T extends Comparable<T>> void fastSort(Integer[] array) {
+    public static <T extends Comparable<T>> void fastSort(T[] array) {
         quickSort(array, 0, array.length - 1);
     }
 
-    public static <T extends Comparable<T>> void quickSort(Integer[] array, int start, int end){
-        if(start < end){
+    private static <T extends Comparable<T>> void quickSort(T[] array, int start, int end) {
+        if (start < end) {
             int pivotIndex = partition(array, start, end);
             quickSort(array, start, pivotIndex - 1);
             quickSort(array, pivotIndex + 1, end);
         }
     }
 
-    private static <T extends Comparable<T>> int partition(Integer[] array, int start, int end){
-        int pivot = array[end];
+    private static <T extends Comparable<T>> int partition(T[] array, int start, int end) {
+        T pivot = array[end];
         int i = start - 1;
-        for(int j = start; j < end; j++){
-            if(array[j] < pivot){
+        for (int j = start; j < end; j++) {
+            if (array[j].compareTo(pivot) < 0) {
                 i++;
-                int tmp = array[i];
+                T tmp = array[i];
                 array[i] = array[j];
                 array[j] = tmp;
             }
         }
-        int tmp = array[i + 1];
-        array[i + 1] = array[end];
-        array[end] = tmp;
-        return i + 1;
-    }
-
-
-
-    public static <T extends Comparable<T>> void fastSort(String[] array) {
-        quickSort(array, 0, array.length - 1);
-    }
-
-    public static <T extends Comparable<T>> void quickSort(String[] array, int start, int end){
-        if(start < end){
-            int pivotIndex = partition(array, start, end);
-            quickSort(array, start, pivotIndex - 1);
-            quickSort(array, pivotIndex + 1, end);
-        }
-    }
-
-    private static <T extends Comparable<T>> int partition(String[] array, int start, int end){
-        String pivot = array[end];
-        int i = start - 1;
-        for(int j = start; j < end; j++){
-            if(array[j].compareTo(pivot) < 0){
-                i++;
-                String tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
-            }
-        }
-        String tmp = array[i + 1];
+        T tmp = array[i + 1];
         array[i + 1] = array[end];
         array[end] = tmp;
         return i + 1;
